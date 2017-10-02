38c61d07985c45a73dbf4580a88baa0fa43c0da7
Nadeem Jamali <Nadeem.Jamali@dealer.com>
2017-10-03 00:14:01 +0500

    add post users login route

--- server/models/user.js
+++ server/models/user.js
@@ -78,6 +78,32 @@ UserSchema.statics.findByToken = function(token){
 
 };
 
+UserSchema.statics.findByCredentials = function(email, password){
+    var User = this;
+
+    return User.findOne({email}).then((user)=>{
+        if(!user){
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject)=>{
+            bcrypt.compare(password, user.password, (err, res)=>{
+                if(err){
+                    return reject(err);
+                }
+                if(res){
+                    resolve(user);
+                }
+                else{
+                    return reject();
+                }
+                
+            });
+        });
+    }).catch((e)=>{
+
+    });
+};
 
 //mongose middlewares
 UserSchema.pre('save',function(next){
